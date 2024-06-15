0f67bddff00b43a0214adbf969d9a925b91446eb
shakil-121 <shahadatshakil76@gmail.com>
2024-06-15 13:32:18 +0600

    adsd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <div className="mt-11">
       <h1 className="text-center font-bold uppercase text-3xl font-sans">WELCOME TO <span className="text-lime-400 ">MEDI-ASK</span></h1> 
-
     </div>
   );
 };
