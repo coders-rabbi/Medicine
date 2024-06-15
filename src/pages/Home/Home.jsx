b9ed49746771bd1ac522a21ecdb7620df84cb813
shakil-121 <shahadatshakil76@gmail.com>
2024-06-15 12:58:30 +0600

    another commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="mt-11">
-      <h1 className="text-center font-bold uppercase text-3xl font-sans">Welcome to <span className="text-lime-400 ">MEDI-ASK</span></h1> 
+      <h1 className="text-center font-bold uppercase text-3xl font-sans">WELCOME TO <span className="text-lime-400 ">MEDI-ASK</span></h1> 
 
     </div>
   );
